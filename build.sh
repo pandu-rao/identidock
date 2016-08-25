56b7fe42e44c9367d563a698ca728509f528601a
Pandu Rao <pandu.rao@gmail.com>
2016-08-25 14:44:52 -0700

    Streamlined build script

--- build.sh
+++ build.sh
@@ -1,45 +1,49 @@
 #!/bin/bash
 
-DOCKER_REGISTRY_USERNAME="$1"
-DOCKER_REGISTRY_PASSWORD="$2"
+DOCKER_REGISTRY_USERNAME="$1";
+DOCKER_REGISTRY_PASSWORD="$2";
+
+DOCKER_PREFIX="$DOCKER_REGISTRY_USERNAME";
+PROJECT='identidock';
 
 # Default compose args
-COMPOSE_ARGS=" -f build.yml -p identidock "
+COMPOSE_ARGS=" -f build.yml -p $PROJECT ";
 
 # Make sure old containers are gone
-sudo docker-compose $COMPOSE_ARGS stop
-sudo docker-compose $COMPOSE_ARGS rm --force -v
+sudo docker-compose $COMPOSE_ARGS stop;
+sudo docker-compose $COMPOSE_ARGS rm --force -v;
 
-# build the system
-sudo docker-compose $COMPOSE_ARGS build --no-cache
-sudo docker-compose $COMPOSE_ARGS up -d
+# Build the system
+sudo docker-compose $COMPOSE_ARGS build --no-cache;
+sudo docker-compose $COMPOSE_ARGS up -d;
 
 # Run unit tests
-sudo docker-compose $COMPOSE_ARGS run --no-deps --rm -e ENV=UNIT identidock
-ERR=$?
+sudo docker-compose $COMPOSE_ARGS run --no-deps --rm -e ENV=UNIT "$PROJECT";
+ERR="$?";
 
 # Run system test if unit tests passed
 if [ $ERR -eq 0 ]; then
-  IP=$(sudo docker inspect -f {{.NetworkSettings.IPAddress}} \
-          identidock_identidock_1)
-  CODE=$(curl -sL -w "%{http_code}" $IP:9090/monster/bla -o /dev/null) || true
-  if [ $CODE -ne 200 ]; then
-    echo "Site returned " $CODE
-    ERR=1
-  else
-    echo 'Test passed - Tagging'
-    HASH=$(git rev-parse --short HEAD)
-    sudo docker tag identidock pandurao/identidock:$HASH
-    sudo docker tag identidock pandurao/identidock:newest
-    echo 'Pushing'
-    sudo docker login -u "$DOCKER_REGISTRY_USERNAME" -p "$DOCKER_REGISTRY_PASSWORD"
-    sudo docker push pandurao/identidock:$HASH
-    sudo docker push pandurao/identidock:newest
-  fi
+    IP=$(sudo docker inspect -f {{.NetworkSettings.IPAddress}} "${PROJECT}_${PROJECT}_1");
+    CODE=$(curl -sL -w "%{http_code}" "$IP:9090/monster/bla" -o /dev/null) || true;
+    if [ "$CODE" -ne 200 ]; then
+        echo "Site returned " "$CODE";
+        ERR=1;
+    else
+        # Tag and push images to registry
+        echo 'Test passed - tagging image';
+        HASH=$(git rev-parse --short HEAD);
+        sudo docker tag "$PROJECT" "$DOCKER_PREFIX/$PROJECT:$HASH";
+        sudo docker tag "$PROJECT" "$DOCKER_PREFIX/$PROJECT:newest";
+
+        echo 'Pushing image';
+        sudo docker login -u "$DOCKER_REGISTRY_USERNAME" -p "$DOCKER_REGISTRY_PASSWORD";
+        sudo docker push "$DOCKER_PREFIX/$PROJECT:$HASH";
+        sudo docker push "$DOCKER_PREFIX/$PROJECT:newest";
+    fi
 fi
 
 # Pull down the system
-sudo docker-compose $COMPOSE_ARGS stop
-sudo docker-compose $COMPOSE_ARGS rm --force -v
+sudo docker-compose $COMPOSE_ARGS stop;
+sudo docker-compose $COMPOSE_ARGS rm --force -v;
 
-echo $ERR
+echo "$ERR";
